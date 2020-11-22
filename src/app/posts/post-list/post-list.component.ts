fd9987b6bf2539c166e948a1c6a066dd4e0dee12
6VioletCoder9 <6laith9@gmail.com>
2020-11-22 21:48:22 +0200

    source code

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs'
+import { PageEvent } from '@angular/material/paginator';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+
+  // posts = [
+  //   {title: '1st Post', content: '1st Content'},
+  //   {title: '2nd Post', content: '2nd Content'},
+  //   {title: '3rd Post', content: '3rd Content'}
+  // ]
+
+  posts: Post[] = [];
+  isLoading = false;
+  totalPosts = 0;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
+  currentPage = 1;
+  userIsAuthenticated = false;
+  userId: string;
+  private postsSub: Subscription;
+  private authStatusSub: Subscription;
+
+  constructor(public postsService: PostsService, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    this.userId = this.authService.getUserId();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((postData: {posts: Post[], postCount: number}) => {
+        this.isLoading = false;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
+      });
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+      this.userId = this.authService.getUserId();
+    })
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.postsPerPage = pageData.pageSize
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage)
+  } 
+
+  onDelete(postId: string) {
+    this.isLoading = true;
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    }, () => {
+      this.isLoading = false;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+    this.authStatusSub.unsubscribe();
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://secure-stack.herokuapp.com/api'
+};
